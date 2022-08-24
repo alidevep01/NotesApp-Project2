264bcd7ee756f0fe3fa5911c9922d4337909d004
mohdali0326 <mohammadali0326@gmail.com>
2022-08-23 20:26:56 -0500

    created showPage

--- models/notes.js
+++ models/notes.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const notesSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  name: { type: String, required: true },
+  title: { type: String },
   description: String,
   img: String,
   crossText: Boolean,
